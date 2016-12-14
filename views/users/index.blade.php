c4ddebd8af20849e619636208b29392adb0c29de
Kit-kat1 <Katynichka95@mail.ru>
2016-12-14 12:54:52 +0200

    Fix typo

--- views/users/index.blade.php
+++ views/users/index.blade.php
@@ -28,21 +28,21 @@
     <div class="well">
         <table class="table">
             <thead>
-            <th>Name</th>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Email</th>
             <th>Options</th>
             </thead>
             <tbody>
             @foreach ($users as $user)
                 <tr class="centered">
-                    <td>{!! $user->first_name !!} {!! $user->first_name !!}</td>
+                    <td>{!! $user->first_name !!}</td>
+                    <td>{!! $user->last_name !!}</td>
                     <td>{!! $user->email !!}</td>
                     <td>
                         &nbsp;<a class="btn btn-success" href="{!! URL::route('users.show', array('users' => $user->id)) !!}"><i class="fa fa-file-text"></i> Show</a>
                         @if(isAdmin())
                             &nbsp;<a class="btn btn-info" href="{!! URL::route('users.edit', array('users' => $user->id)) !!}"><i class="fa fa-pencil-square-o"></i> Edit</a>
-                        @endif
-                        @if(isAdmin())
                             &nbsp;<a class="btn btn-default" href="#reset_user_{!! $user->id !!}" data-toggle="modal" data-target="#reset_user_{!! $user->id !!}"><i class="fa fa-lock"></i> Reset Password</a>
                             &nbsp;<a class="btn btn-danger" href="#delete_user_{!! $user->id !!}" data-toggle="modal" data-target="#delete_user_{!! $user->id !!}"><i class="fa fa-times"></i> Delete</a>
                         @endif
